2ef68d50ad5e6614e9186f328dc80253d792cb6b
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-05-11 20:34:40 +0530

    create add friend page UI

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import Button from "@/components/ui/Button";
+import { signIn } from "next-auth/react";
 import { FC, useState } from "react";
+import toast from "react-hot-toast";
 
 interface pageProps {}
 const googleSvg = (
@@ -36,8 +38,17 @@ const googleSvg = (
 );
 
 const Page: FC<pageProps> = ({}) => {
-  async function loginWithGoogle() {}
   const [isLoading, setIsLoading] = useState(false);
+  async function loginWithGoogle() {
+    setIsLoading(true);
+    try {
+      await signIn("google");
+    } catch (error) {
+      toast.error("something went wrong with login");
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
     <>
       <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
@@ -47,7 +58,7 @@ const Page: FC<pageProps> = ({}) => {
             <h2 className='mt-6 text-center text-3xl font-bold tracking-tighter text-gray-900'>Sign in to your account</h2>
           </div>
           <Button isLoading={isLoading} type='button' className='max-w-sm mx-auto w-full' onClick={loginWithGoogle}>
-            {googleSvg}
+            {isLoading ? null : googleSvg}
             Google
           </Button>
         </div>
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth/next";
+import { authOptions } from "../../../lib/auth";
 
-// export default NextAuth(authOptions){
-
-// }
+export default NextAuth(authOptions);
