3a25cbaebdb5459c1d5fa7b62843e617b064fda8
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-05-12 09:46:51 +0530

    add sidebar

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -3,8 +3,8 @@ import { FC } from "react";
 
 const page: FC = ({}) => {
   return (
-    <main className='pt-8 max-w-sm mx-auto'>
-      <h1 className='font-bold text-center text-5xl mb-8'>Add a friend</h1>
+    <main className='pt-8'>
+      <h1 className='font-bold text-5xl mb-8'>Add a friend</h1>
       <AddFriendButton />
     </main>
   );
